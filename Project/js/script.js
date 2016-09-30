f57b695dfdabb170ce8d452b8129efb3e2b04c39
unknown <nehabha@nehabha.cybage.com>
2016-09-30 14:00:28 +0530

    "more changes"

---
+++ Project/js/script.js
@@ -0,0 +1,21 @@
+function onAll(){
+	document.getElementById("img1").style.display = "block";
+	document.getElementById("img2").style.display = "block";
+	document.getElementById("img3").style.display = "block";
+	document.getElementById("img4").style.display = "block";
+	document.getElementById("img5").style.display = "block";
+	document.getElementById("img6").style.display = "block";
+	document.getElementById("img7").style.display = "block";
+	document.getElementById("img8").style.display = "block";
+}
+
+function myFunction(){
+document.getElementById("img1").style.display = "block";
+document.getElementById("img2").style.display = "block";
+document.getElementById("img3").style.display = "block";
+document.getElementById("img4").style.display = "block";
+document.getElementById("img5").style.display = "none";
+document.getElementById("img6").style.display = "none";
+document.getElementById("img7").style.display = "none";
+document.getElementById("img8").style.display = "none";
+}
